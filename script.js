b7802d15d9093ab24ddeb6a7e457dc139d1d2487
manohar <manumanohar1527@gmail.com>
2020-03-30 04:59:14 -0700

    updated guess Checker and Responses

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ button.addEventListener('click', function () {
         mover();
     } else {
         console.log('cheker');
+        const k = document.querySelectorAll('.numb');
+        console.log(k);
+        for (i = 0; i < k.length; i++) {
+
+            //console.log(k[i].value);
+            if (k[i].value == k[i].correct) {
+                console.log("matched");
+            } else {
+                console.log("not matched");
+            }
+        }
     }
 });
 
@@ -17,7 +28,7 @@ button.addEventListener('click', function () {
 function mover() {
     for (i = 0; i < 5; i++) {
         let element = document.createElement('input');
-        console.log(element);
+        //console.log(element);
         element.setAttribute('type', 'number');
         element.style.width = "100px";
         element.style.fontSize = '2rem';
@@ -25,14 +36,20 @@ function mover() {
         element.min = 0;
         element.size = 1;
         element.order = i;
-        element.value = 0;
+        element.correct = Math.floor(Math.random() * 10);
+        element.value = element.correct;
+        element.classList.add('numb');
+
+        //console.log(element.value);
         element.style.marginLeft = '30px';
         gameArea.appendChild(element);
     }
 }
 
+
 //adding styles to the button
 function btnStyle() {
+    gameArea.style.display = 'flex';
     gameArea.style.marginLeft = '350px';
     gameArea.style.marginTop = '50px';
     button.style.border = 'none';
@@ -42,7 +59,7 @@ function btnStyle() {
     button.style.width = '200px';
     button.style.padding = '10px';
     button.style.outline = 'none';
-    button.style.margin = '250px 0 0 600px';
+    button.style.margin = '250px 0 0 550px';
     button.style.border = '1px solid black';
     button.addEventListener('mouseover', function () {
         this.style.backgroundColor = 'black';
